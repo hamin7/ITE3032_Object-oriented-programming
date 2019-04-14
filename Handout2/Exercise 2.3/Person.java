27e2eef50777bea5a505af64681bca0551a6fd37
hamin7 <33690039+hamin7@users.noreply.github.com>
2019-04-14 17:24:54 +0900

    Update Person.java

--- Handout2/Exercise 2.3/Person.java
+++ Handout2/Exercise 2.3/Person.java
@@ -1,10 +1,10 @@
 public class Person{
-  public String firstName;
-  public String lastName;
-  public Address address;
+  public String firstName;      // public String 타입의 이름.
+  public String lastName;     // public String 타입의 성.
+  public Address address;     // public Address 타입의 주소.
 
-  @Override
-  public String toString(){
+  @Override         // 오버라이딩 해서.
+  public String toString(){     // 객체가 가진 정보를 문자열로 반환하는 메소드.
     //TODO Auto-generated method stub
     return firstName+lastName+address.getStreet();
   }
